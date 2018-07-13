fa0661a55c514c46c9b59a61e6b79f02d697c04c
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-07-13 13:57:15 -0500

    Remplazo

--- Y2K/Y2K/Models/Weather.cs
+++ Y2K/Y2K/Models/Weather.cs
@@ -7,10 +7,5 @@ namespace Y2K.Models
 {
     public class Weather
     {
-        public int Id { get; set; }
-        public int IdExternal { get; set; }
-        public string Main { get; set; }
-        public string Description { get; set; }
-        public string Icon { get; set; }
     }
 }
